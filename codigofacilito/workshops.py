297703201887ca5e6ad2070ab3291fd600fcb573
Ramsespf <ramses.fagundo@gmail.com>
2022-06-01 16:49:09 -0400

    Final commit

--- codigofacilito/workshops.py
+++ codigofacilito/workshops.py
@@ -2,6 +2,11 @@ from urllib import response
 import requests
 
 def unreleased():
+    """Retorna los proximos talleres de codigo facilito
+
+    >>> type(unreleased()) == type(dict())
+    True
+    """
     response = requests.get("https://codigofacilito.com/api/v2/workshops/unreleased")
 
     if response.status_code == 200:
