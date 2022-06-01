e625d14f54df53be642c06743785bf54b04eff4a
Ramsespf <ramses.fagundo@gmail.com>
2022-06-01 16:25:46 -0400

    Modulo logging

--- codigofacilito/__main__.py
+++ codigofacilito/__main__.py
@@ -1,8 +1,22 @@
 from codigofacilito import unreleased
 
-if __name__ == "__main__":
-    print(">>> Estamos comenzando la ejecucion del paquete")
-    workshops = unreleased()
-    print(workshops)
+#Forma profecional de imprimir en pantalla
+import logging
 
-    print(">>> Estamos finalizando la ejecucion del paquete")
+"""
+INFO -> 10
+DEBUG -> 20
+WARNING -> 30
+ERROR -> 40
+CRITICAL -> 50
+"""
+#para q en consola se imprima desde el lvl DEBUG x default es desde WARNING
+
+logging.basicConfig(level=logging.DEBUG)
+
+if __name__ == "__main__":
+    logging.debug(">>> Estamos comenzando la ejecucion del paquete")
+    workshops = unreleased()
+    logging.debug(workshops)
+
+    logging.debug(">>> Estamos finalizando la ejecucion del paquete")
